9d6374f96b526b8d641895f5c2025229a37894b0
Rabia <Tabassumzzi@gmail.com>
2020-09-17 19:14:14 -0500

    pushing the changes Github

---
+++ mmp/src/main/java/org/iit/mmp/helper/HelperClass.java
@@ -0,0 +1,83 @@
+package org.iit.mmp.helper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class HelperClass {
+WebDriver driver;
+	public HelperClass(WebDriver driver) 
+	{
+		this.driver= driver;
+	
+		
+	
+		
+	}
+
+	
+public void login(String uname, String pword, String url) {
+	driver.get(("http://96.84.175.78/MMP-Release2-Integrated-Build.6.8.000/portal/login.php"));
+	driver.findElement(By.id("username")).sendKeys("ria1");
+	driver.findElement(By.id("password")).sendKeys("Ria12345");
+	driver.findElement(By.name("submit")).click();
+}
+
+public void navigateToAModule(String moduleName)
+{
+	driver.findElement(By.xpath("//span[contains(text(),'"+moduleName+"')]")).click();
+	
+}
+public void launchBrowser(String url) {
+	driver.get(url);
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+}
+
+public void adminLogin(String uName, String password){
+
+	driver.findElement(By.id("username")).sendKeys("Thomas_444");
+	driver.findElement(By.id("password")).sendKeys("Edison_444");
+	driver.findElement(By.name("admin")).click();
+}
+
+
+public void adminModuleNavigation(String moduleName){
+
+	driver.findElement(By.xpath("//span[contains(text(),'"+moduleName+"')]")).click();
+}
+public void captureScreenshot(String tc_Name) throws IOException
+{
+	System.out.println("Inside Capturing Screenshot method");
+	TakesScreenshot tsh = (TakesScreenshot)driver;
+	File sourceFile = tsh.getScreenshotAs(OutputType.FILE);
+	System.out.println(sourceFile.getAbsolutePath());
+	String destinationPath = System.getProperty("user.dir")+"\\screenshots\\"+tc_Name+"_"+
+										Calendar.getInstance().getTimeInMillis()%1000000000+".jpg";
+	File destFile = new File(destinationPath); 
+	FileUtils.copyFile(sourceFile,destFile);
+	System.out.println(destinationPath);
+	System.out.println("Exiting Screenshot");
+	
+}
+public void highLightElement(WebElement ele){
+	JavascriptExecutor js =(JavascriptExecutor)driver;
+	js.executeScript("arguments[0].setAttribute('style', 'background:yellow; border:2px solid red;')", ele);
+
+
+
+	
+}
+public void closeDriver() {
+	//driver.findElement(By.xpath("//span[contains(text(), 'Logout')]")).click();
+	driver.close();
+}
+}
---
+++ mmp/src/main/java/org/iit/mmp/patientmodule/pages/LoginPage.java
@@ -0,0 +1,26 @@
+package org.iit.mmp.patientmodule.pages;
+
+
+import org.iit.util.BaseClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage  extends BaseClass {
+	WebDriver driver;
+	public LoginPage() {
+		
+		this.driver = driver;
+	
+		
+		
+		
+	
+
+//}
+//	public void login(String uname, String pword, String url) {
+//		driver.get(("http://96.84.175.78/MMP-Release2-Integrated-Build.6.8.000/portal/login.php"));
+//		driver.findElement(By.id("username")).sendKeys("ria1");
+//		driver.findElement(By.id("password")).sendKeys("Ria12345");
+//		driver.findElement(By.name("submit")).click();
+	}
+}
